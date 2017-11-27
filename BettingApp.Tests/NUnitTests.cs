4c8a1524ea45c44ad1528fe788292d8b39df2733
JD Kiely <KielyJD@GMail.com>
2017-11-27 17:24:50 +0000

    Added NUnit Testing

---
+++ BettingApp.Tests/NUnitTests.cs
@@ -0,0 +1,30 @@
+﻿using BettingApp.BettingApp.Core;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BettingApp.Tests
+{
+    [TestFixture]
+    class NUnitTests
+    {
+        [Test]
+        public void TestCalculateListPayout()
+        {
+            List<Bet> testList = new List<Bet> { new Bet
+            {
+                Stake = 1.1,
+                Price = 2.2
+            } };
+
+            testList = Utilities.CalculateListPayout(testList);
+
+            var expectedResult = testList.FirstOrDefault().Payout;
+
+            Assert.That(expectedResult, Is.EqualTo(1.1*2.2));
+        } 
+    }
+}
